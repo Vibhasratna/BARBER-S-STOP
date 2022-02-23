174545cafb75fa5c72b9f3f5bb8710be16950582
Vibhas <100060723+Vibhasratna@users.noreply.github.com>
2022-02-23 19:47:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+function log(){
+  window.open('Chrome Extension')
+}
+
+
+function article() {
+  
+  window.open('https://economictimes.indiatimes.com/news/india/covid-19-curbs-salon-owners-barbers-seek-aid-from-maharashtra-govt/articleshow/82083862.cms');
+}
+function trend() {
+
+  window.open('https://www.shutterstock.com/search/indian+barber+shop');
+}
+function video() {
+
+  window.open('https://www.youtube.com/channel/UCkn4vbFbK3yTBxHlL97u6nQ');
+}
+function yout(){
+  window.open('https://www.youtube.com/channel/UCkn4vbFbK3yTBxHlL97u6nQ')
+}
+function inst(){
+  window.open('')
+}
+function face(){
+  window.open('')
+}
+
